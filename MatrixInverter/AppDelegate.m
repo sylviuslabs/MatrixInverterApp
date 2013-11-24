a3845c9ad570bf87a21a31702041ac6ada3dbccb
Gregory Haskins <greg@greghaskins.com>
2013-11-23 23:18:36 -0500

    AppDelegate now creates a MainViewController and sets its as root

--- MatrixInverter/AppDelegate.m
+++ MatrixInverter/AppDelegate.m
@@ -1,12 +1,22 @@
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    const struct CGRect mainScreenBounds = [[UIScreen mainScreen] bounds];
-    UIWindow *const window = [[UIWindow alloc] initWithFrame:mainScreenBounds];
+    UIWindow *const window = [self buildWindow];
+
+    MainViewController *const mainViewController = [[MainViewController alloc] init];
+    [window setRootViewController:mainViewController];
+
     [self setWindow:window];
     return YES;
+}
+
+- (UIWindow * const)buildWindow {
+    const struct CGRect mainScreenBounds = [[UIScreen mainScreen] bounds];
+    UIWindow *const window = [[UIWindow alloc] initWithFrame:mainScreenBounds];
+    return window;
 }
 
 @end
--- MatrixInverterTests/AppDelegateSpec.m
+++ MatrixInverterTests/AppDelegateSpec.m
@@ -1,5 +1,6 @@
 #import "SLTestingKit.h"
 #import "AppDelegate.h"
+#import "MainViewController.h"
 
 SPEC_BEGIN(AppDelegateSpec)
 
@@ -9,22 +10,26 @@ SPEC_BEGIN(AppDelegateSpec)
 
             beforeEach(^{
                 appDelegate = [[AppDelegate alloc] init];
+                [appDelegate application:nil didFinishLaunchingWithOptions:nil];
             });
+
 
             it(@"shoud conform to UIApplicationDelegate protocol", ^{
                 assertThat(appDelegate, conformsTo(@protocol(UIApplicationDelegate)));
             });
 
             it(@"shoud create a window when launching", ^{
-                [appDelegate application:nil didFinishLaunchingWithOptions:nil];
                 assertThat([appDelegate window], instanceOf([UIWindow class]));
             });
 
             it(@"should have a window that fills the bounds of the screen", ^{
-                [appDelegate application:nil didFinishLaunchingWithOptions:nil];
                 NSValue *const windowFrame = [NSValue valueWithCGRect:[[appDelegate window] frame]];
                 NSValue *const mainScreenBounds = [NSValue valueWithCGRect:[[UIScreen mainScreen] bounds]];
                 assertThat(windowFrame, equalTo(mainScreenBounds));
+            });
+
+            it(@"should set up MainViewController as the root view of the window", ^{
+                assertThat([[appDelegate window] rootViewController], instanceOf([MainViewController class]));
             });
 
         });
